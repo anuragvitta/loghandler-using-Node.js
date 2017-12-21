86b46e674cdd51f3b28573945283f71766b0a409
anuragvitta <33395979+anuragvitta@users.noreply.github.com>
2017-12-21 12:38:10 +0530

    Update deletefiles.js

--- deletefiles.js
+++ deletefiles.js
@@ -2,4 +2,3 @@ let fs = require('fs');
 let findRemoveSync = require('find-remove');
 let path='H:/node/filesystem/file/';
 var result = findRemoveSync(path, {age: {seconds: 604800},extensions:'.txt'});
-console.log(result);
